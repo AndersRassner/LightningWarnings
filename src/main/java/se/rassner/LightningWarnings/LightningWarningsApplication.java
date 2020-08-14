9a9d4b01df50867d42ffe89464177eb694722c59
Anders Rassner <andra248@student.liu.se>
2020-08-14 16:45:20 +0200

    Filtering strikes on lat/lon. Need to add GregorianCalendar stuff to actually get today's strikes.

--- src/main/java/se/rassner/LightningWarnings/LightningWarningsApplication.java
+++ src/main/java/se/rassner/LightningWarnings/LightningWarningsApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.net.URI;
 import java.net.http.*;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.Iterator;
+
 import com.google.gson.*;
 
 @SpringBootApplication
@@ -33,21 +35,37 @@ public class LightningWarningsApplication {
 		Transform received data to workable format, if needed
 		*/
 		JsonElement parser = JsonParser.parseString(response.body());
-		JsonArray Strikes = parser.getAsJsonObject().getAsJsonArray("values");
-		//System.out.println(Strikes.toString());
+		JsonArray strikes = parser.getAsJsonObject().getAsJsonArray("values");
+		System.out.println(strikes.get(0).toString());
 
 		/*
 		Set area to search, starting with default, then by asking through interface
 		*/
-		
+		LatLonRectangle malmoArea = new LatLonRectangle(55.48, 55.65, 12.75, 13.23);
 		
 		/*
 		Filter data using given area
 		*/
-		
+		JsonArray filteredStrikes = new JsonArray();
+		Iterator<JsonElement> strikesIt = strikes.iterator();
+		while(strikesIt.hasNext()) {
+			JsonObject strike = (JsonObject) strikesIt.next();
+			double lat = strike.get("lat").getAsDouble();
+			double lon = strike.get("lon").getAsDouble();;
+			if(malmoArea.contains(new LatLonCoordinate(lat,lon))) { 
+				filteredStrikes.add(strike);
+			}
+		}
+		System.out.println(filteredStrikes.toString());
+
 		/*
 		Show result, starting with println, then by showing in interface
 		*/
+		System.out.println("Number of strikes in given area: " + filteredStrikes.size());
+		if(filteredStrikes.size() > 0) {
+			String latestStrike = "00:00"; // replace with GregorianCalendar using datetime in JsonObjects in filteredStrikes
+			System.out.println("Latest strike occured at: " + latestStrike);
+		}
 		System.out.println("End of program reached");
 	
 	}
